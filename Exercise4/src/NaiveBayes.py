269110b8fb12952176f0177c2f42814020dbe918
email.mufl <email.mufl@googlemail.com>
2011-06-11 12:11:22 +0000

    war im falschen Ordner -.-

---
+++ Exercise4/src/NaiveBayes.py
@@ -0,0 +1,18 @@
+'''
+Created on 11.06.2011
+
+@author: m0ep
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
